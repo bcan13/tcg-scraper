4916fe9378f3ffd4f2ceccfefe65cb9d362d8f07
Brian Can <canbrian59@gmail.com>
2025-02-13 00:50:28 -0800

    updated requirements and removed sensitive info from config

--- config/config.py
+++ config/config.py
@@ -1,10 +1,3 @@
-# Apollo.io API Key
-APOLLO_API_KEY = ""
-
-# Email Configuration
-EMAIL_SENDER = ""
-EMAIL_PASSWORD = ""  # Use an App Password for security
-
 # Outreach Details
 OUR_NAME = ""
 SIGNIFICANT_VALUE = "data-driven insights to enhance decision-making"
