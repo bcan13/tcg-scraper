a0fbb5bb4459e2bafe48ef4052652f797c22d72e
Brian Can <canbrian59@gmail.com>
2025-02-13 22:27:30 -0800

    added todo's

--- handlers/wellfound.py
+++ handlers/wellfound.py
@@ -3,7 +3,8 @@ import pandas as pd
 import csv
 import os
 
-# TODO: add location column
+# TODO: format better for remote
+# TODO: add a filter for company size
 
 
 async def get_jobs_wellfound():
@@ -26,6 +27,7 @@ async def get_jobs_wellfound():
     all_companies = []
 
     for job in job_titles:
+        
         search_url = f"https://wellfound.com/role/l/{job.replace(' ', '-')}/{location.replace(' ', '-')}"
         page = await browser.get(search_url)
         
@@ -48,7 +50,40 @@ async def get_jobs_wellfound():
                 "company_name": company_name.text if company_name else "",
                 "description": company_desc.text.strip('"') if company_desc else "",
                 "job_type": job,
-                "size": company_size.text if company_size else ""
+                "size": company_size.text if company_size else "",
+                "location": location
+            }
+
+            # append the company data to the list
+            all_companies.append(company_data)
+
+
+    for job in job_titles:
+        
+        search_url = f"https://wellfound.com/role/r/{job.replace(' ', '-')}"
+        page = await browser.get(search_url)
+        
+        await page.wait_for(selector = '.pl-2.flex.flex-col')
+
+        # get company div for each company on each job
+        company_div = await page.query_selector_all('.pl-2.flex.flex-col')  # Adjust selectors if needed
+        
+        for company in company_div:
+            company_name = await company.query_selector('h2.inline.text-md.font-semibold')
+            company_desc = await company.query_selector('span.text-xs.text-neutral-1000')
+            company_size = await company.query_selector('span.text-xs.italic.text-neutral-500')
+            
+            # if we already seen the company, continue
+            if company_name.text in existing_companies:
+                continue
+
+            # store the data for the current company
+            company_data = {
+                "company_name": company_name.text if company_name else "",
+                "description": company_desc.text.strip('"') if company_desc else "",
+                "job_type": job,
+                "size": company_size.text if company_size else "",
+                "location": "remote"
             }
 
             # append the company data to the list
